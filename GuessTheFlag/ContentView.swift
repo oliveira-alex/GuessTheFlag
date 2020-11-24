b6d2eab6356026fd16741bde0890e58090e136ec
Alex Oliveira <alexdeoliveira.poli@gmail.com>
2020-11-24 12:39:43 -0300

    End of Part 2 - Colors and frames

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,13 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 		ZStack(alignment: .topTrailing) {
+			Color.yellow.edgesIgnoringSafeArea(.all)
+			Color.green.frame(width: 250, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
 			Text("Hello, world!")
 				.font(.largeTitle)
+				.background(Color.gray)
 			Text("This is inside a stack")
 				.background(Color.red)
 				.foregroundColor(.blue)
 		}
-		.background(Color.gray)
     }
 }
 
