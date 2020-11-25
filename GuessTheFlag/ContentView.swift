24255e2d4074dcc5a4928813bb25430d43e7fd1c
Alex Oliveira <alexdeoliveira.poli@gmail.com>
2020-11-25 20:25:20 -0300

    1/3 Challenge complete - Added an @State property to store the user's score

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
 	@State private var showingScore = false
 	@State private var scoreTitle = ""
 	
+	@State private var score = 0
+	
     var body: some View {
 		ZStack {
 			LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
@@ -46,7 +48,7 @@ struct ContentView: View {
 			}
 		}
 		.alert(isPresented: $showingScore) {
-			Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+			Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
 				self.askQuestion()
 			})
 		}
@@ -55,8 +57,10 @@ struct ContentView: View {
 	func flagTapped(_ number: Int) {
 		if number == correctAnswer {
 			scoreTitle = "Correct"
+			score += 1
 		} else {
 			scoreTitle = "Wrong"
+			score -= 1
 		}
 		
 		showingScore = true
